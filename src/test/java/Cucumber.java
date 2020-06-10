6a91548b10464d0a2847438339d07727fb00177a
x212498 <renatolessa.2011@hotmail.com>
2020-06-10 20:52:06 -0300

    first commit cucumber

---
+++ src/test/java/Cucumber.java
@@ -0,0 +1,23 @@
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class Cucumber {
+	
+
+@Given("^creat file$")
+public void creat_file() throws Throwable {
+  
+}
+
+@When("^Run$")
+public void run() throws Throwable {
+   
+}
+
+@Then("^The especification must with success$")
+public void the_especification_must_with_success() throws Throwable {
+   
+}
+
+}
---
+++ target/test-classes/cucumber.feature
@@ -0,0 +1,11 @@
+Feature: Cucumber
+
+		I want run cucumber
+		
+
+Scenario: Must run espcifications 
+
+		Given creat file
+		When 	Run 
+		Then The especification must with success 
+		
