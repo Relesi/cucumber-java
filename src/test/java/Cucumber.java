f372862c2b80478b94edc3810d3173bddb196c63
x212498 <renatolessa.2011@hotmail.com>
2020-06-11 11:24:13 -0300

    work with regex

--- src/test/java/Cucumber.java
+++ src/test/java/Cucumber.java
@@ -1,3 +1,8 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 import org.junit.Assert;
 
 import cucumber.api.java.pt.Dado;
@@ -9,39 +14,73 @@ public class Cucumber {
 	@Dado("^creat file$")
 	public void creatFile() throws Throwable {
 		System.out.println("qualquer coisa");
-		
+
 	}
 
 	@Quando("^Run$")
 	public void run() throws Throwable {
-		
+
 	}
 
 	@Então("^The especification must with success$")
 	public void theEspecificationMustWithSuccess() throws Throwable {
-		
+
 	}
-	
+
 	private int contador = 0;
-	
+
 	@Dado("^that the contactor value  is (\\d+)$")
 	public void that_the_contactor_value_is(int arg1) throws Throwable {
-	  contador = arg1;
+		contador = arg1;
 	}
 
 	@Quando("^I increment in (\\d+)$")
 	public void i_increment_in(int arg1) throws Throwable {
-		contador  = contador + arg1;
-	   
+		contador = contador + arg1;
+
 	}
 
 	@Então("^The value contactor It will be (\\d+)$")
 	public void the_value_contactor_It_will_be(int arg1) throws Throwable {
-//	   System.out.println(arg1);
-//	   System.out.println(contador);
-//	   System.out.println(arg1 == contador);
-	   //Assert.assertTrue(arg1 == contador);
-	   Assert.assertEquals(contador, arg1);
+		Assert.assertEquals(contador, arg1);
+	}
+
+	Date entrega = new Date();
+
+	@Dado("^que a entrega é dia (\\d+)/(\\d+)/(\\d+)$")
+	public void queAEntregaÉDia(int dia, int mes, int ano) throws Throwable {
+		Calendar cal = Calendar.getInstance();
+		cal.set(Calendar.DAY_OF_MONTH, dia);
+		cal.set(Calendar.MONTH, mes - 1);
+		cal.set(Calendar.YEAR, ano);
+		entrega = cal.getTime();
+		// fazer apresentação do mes
+
+	}
+
+	// ^a entrega atrasar em (\\d+) dias$
+	//^a entrega atrasar em (\\d+) (.+)$
+	
+	@Quando("^a entrega atrasar em (\\d+) (dia|dias|mes|meses)$")
+	public void aEntregaAtrasarEmDias(int arg1, String tempo) throws Throwable {
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(entrega);
+		if (tempo.equals("dias")) {
+			cal.add(Calendar.DAY_OF_MONTH, arg1);
+		}
+		
+		if (tempo.equals("meses")) {
+			cal.add(Calendar.MONTH, arg1);
+		}
+
+		entrega = cal.getTime();
+	}
+
+	@Então("^a entrega sera efetuada em (\\d{2}\\/\\d{2}\\/\\d{4})$")
+	public void aEntregaSeraEfetuadaEm(String data) throws Throwable {
+		DateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+		String dataFormatada = format.format(entrega);
+		Assert.assertEquals(data, dataFormatada);
 	}
 
 }
--- target/test-classes/cucumber.feature
+++ target/test-classes/cucumber.feature
@@ -14,8 +14,20 @@ Cenário: Must increment the contactor
 	Quando I increment in 3
 	Então The value contactor It will be 18
 	
-
+Cenário: Must increment the contactor
+	Dado that the contactor value  is 123
+	Quando I increment in 35
+	Então The value contactor It will be 158
+	
+Cenário: Deve calcular atraso na entrega 
+	Dado que a entrega é dia 05/04/2018
+	Quando a entrega atrasar em 2 dias
+	Então a entrega sera efetuada em 07/04/2018
 		
+Cenário: Deve calcular atraso no prazo de entrega china
+	Dado que o prazo é dia 05/04/2018
+	Quando a entrega atrasar em 2 meses  
+	Então a entrega sera efetuada em 05/06/2018
 		
 		
 		
