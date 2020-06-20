81ba373552175af2ff95df4b31875e7b7a9c24be
x212498 <renatolessa.2011@hotmail.com>
2020-06-20 11:08:02 -0300

    to do integation

--- src/test/java/com/renato/runners/RunnerFuncionalTest.java
+++ src/test/java/com/renato/runners/RunnerFuncionalTest.java
@@ -1,8 +1,5 @@
 package com.renato.runners;
 import org.junit.runner.RunWith;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.SnippetType;
@@ -21,15 +18,6 @@ import cucumber.api.junit.Cucumber;
 		)
 public class RunnerFuncionalTest {
 	
-	//@BeforeClass
-	public static void reset() {
-		WebDriver driver = new ChromeDriver();
-		driver.get("https://srbarriga.herokuapp.com");
-		driver.findElement(By.id("email")).sendKeys("rock@rock");
-		driver.findElement(By.name("senha")).sendKeys("rock");
-		driver.findElement(By.tagName("button")).click();
-		driver.findElement(By.linkText("reset")).click();
-		driver.quit();
-	}
+
 
 }
